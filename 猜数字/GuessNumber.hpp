94d1a662bab763979273540c34aeaad5922e1a8d
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2020-05-29 19:33:30 +0800

    Update GuessNumber.hpp

--- 猜数字/GuessNumber.hpp
+++ 猜数字/GuessNumber.hpp
@@ -1,19 +1,19 @@
-typedef unsigned long int number4;
+typedef unsigned short int number4;
 
 inline number4 Digit1(number4 x){
-  return x & 0xFF000000;
+  return x & 0xF000;
 }
 
 inline number4 Digit2(number4 x){
-  return x & 0x00FF0000;
+  return x & 0x0F00;
 }
 
 inline number4 Digit3(number4 x){
-  return x & 0x0000FF00;
+  return x & 0x00F0;
 }
 
 inline number4 Digit4(number4 x){
-  return x & 0x000000FF;
+  return x & 0x000F;
 }
 
 int RightPosition(number4 x, number4 y){
@@ -25,6 +25,14 @@ inline int WrongPosition(number4 x,number4 y){
   return RightPosition(x, y>>4|y<<12) + RightPosition(x, y>>8|y<<8) + RightPosition(x,y>>12|y<<4);
 }
 
-inline bool IsValidGuess(number4 x){
-  return WrongPosition(x,x) ? false : true;
+bool DoesRepeat(number4 x){
+  return (bool) WrongPosition(x,x);
+}
+
+bool AreDigitsValid(number4 x){
+  return Digit1(x)<=0x9000 && Digit2(x)<=0x0900 && Digit3(x)<=0x0090 && Digit4(x)<=0x0009;
+}
+
+bool IsValidGuess(number4 x){
+  return !DoesRepeat(x) && AreDigitsValid(x);
 }
